fc2129c69a70e1a0bf051f442cc9302a2ffa6521
andrewrosario <rosario.andrew@gmail.com>
2019-09-23 13:35:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+const findMatching = (drivers, string) => {
+    return drivers.filter( d => {
+        return d.toUpperCase() === string.toUpperCase()
+    })
+}
+
+const fuzzyMatch = (drivers, string) => {
+    return drivers.filter( d => {
+        console.log(string)
+        let myReg = new RegExp('^' + string)
+        return myReg.test(d)
+    })
+}
+
+const matchName = (drivers, string) => {
+    return drivers.filter( d => {
+        return d['name'] === string
+    })
+}
